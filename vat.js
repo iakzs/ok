99d5ea36585431aa4dbbcf7ecd8c9dcdd45163aa
not <105176207+iakzs@users.noreply.github.com>
2023-03-03 22:13:15 -0300

    Create vat.js

---
+++ vat.js
@@ -0,0 +1,13 @@
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({ auth: process.env.TOKAN });
+
+// make a request to get the current user's profile
+octokit.users.getAuthenticated()
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+var accesstokenn = process.env.TOKAN;
