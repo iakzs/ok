1d29935893414cd032178f20f87457c2182c37fc
not <105176207+iakzs@users.noreply.github.com>
2023-03-03 22:06:30 -0300

    Update vat.js

--- vat.js
+++ vat.js
@@ -1,6 +1,6 @@
 var tkn = process.env.TOKAN
 const { Octokit } = require("@octokit/rest");
-const octokit = new Octokit({ auth: tkn });
+const octokit = new Octokit({ auth: process.env.TOKAN });
 
 // make a request to get the current user's profile
 octokit.users.getAuthenticated()
