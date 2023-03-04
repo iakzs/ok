c6935d640a222d2f5b2f7f5a636ba7da39099e7f
not <105176207+iakzs@users.noreply.github.com>
2023-03-04 10:26:43 -0300

    Update epicthing.js

--- epicthing.js
+++ epicthing.js
@@ -3,7 +3,7 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 const octokit = new Octokit({ auth: process.env.TOKAN });
 
 // make a request to get the current user's profile
-octokit.users.getAuthenticated()
+octokit.rest.users.getAuthenticated()
   .then(response => {
     console.log(response.data);
   })
